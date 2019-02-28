82beaf02063a968ed9c1d643016257cc36d1fecf
Stig Terrebonne <stigterrebonne@gmail.com>
2019-02-28 09:30:36 -0800

    Finish everything

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'MRpwb9ExVKxLhg6FS32SM9lfoPsLmCTU';
+
+// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  		center: [32.8788033, -117.2381008],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
